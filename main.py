1b04a374ed8bcf749a210344b62a5334400beceb
XueYuan Qiao <xueyuanqiao@XueYuanMacBook-Pro.local>
2026-01-30 07:55:15 +0800

    update

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 from supabase import create_client, Client
 import os
 
 app = FastAPI()
 
+templates = Jinja2Templates(directory="templates")
+
 url = os.environ.get("SUPABASE_URL")
 key = os.environ.get("SUPABASE_ANON_KEY")
 supabase: Client = create_client(url, key)
+
+@app.get("/", response_class=HTMLResponse)
+async def read_root(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
 
 @app.get("/users")
 async def get_users():
